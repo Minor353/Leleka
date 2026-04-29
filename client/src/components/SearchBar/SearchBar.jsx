d52313b8f897b072bfa4eaf2c029ba33e05ce029
Minor <nikitenkogv.main@gmail.com>
2026-04-29 08:22:46 +0300

    Додана стилізация User bar компонентів

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
+import style from './style.module.scss'
+
 export default function SearchBar() {
   return (
-    <div>
-        <form action="#" method="get">
-            <input type="search" name="search" placeholder='Пошук...' />
+    <div className={style['search-bar']}>
+        <form className={style['search-bar__form']} action="#" method="get">
+            <input className={style['search-bar__input']} type="search" name="search" placeholder='Пошук...' />
         </form>
     </div>
   )
